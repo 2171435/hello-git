cfe8f456fee61c2883c43781f9dad8fdf1739f38
2171435 <2171435@hansung.ac.kr>
2024-11-10 15:16:37 +0000

    쓸데없는 문구가 추가된

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
+/*
+아무거나 적어보는 달라진 점
+정말 아무거나 적는거라 별 내용 없고
+뭐가 추가됐다는 사실만 있으면 되는거라
+큰 의미도 없는 문구임.
+*/
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world from GIT!");
         System.out.println("Changes after First Commit!");
     }
 }
+
